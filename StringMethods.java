1e12f60fede8c8d1993fc48759961587a2e58738
unknown <mugglereddy@gmail.com>
2018-03-16 10:45:22 -0500

    String methods

---
+++ StringMethods.java
@@ -0,0 +1,18 @@
+package com.sageit.javaprograms;
+
+public class StringMethods {
+
+	public static void main(String[] args) {
+String X = "Sumanth";
+System.out.println("Hello " + X );
+System.out.println(X.isEmpty());
+System.out.println(X.charAt(1));
+System.out.println(X.startsWith(X));
+System.out.println(X.toUpperCase());
+System.out.println(X.substring(1,4));
+System.out.println(X.replace("S", "Y"));
+
+
+	}
+
+}
